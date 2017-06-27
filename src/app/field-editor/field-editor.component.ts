373eef0b1439025797c0d344715daaf82fc8f6c7
Lehajam <lehajam@gmail.com>
2017-06-27 12:53:21 +1000

    Changing selector name Reset input text after value saved

--- src/app/field-editor/field-editor.component.ts
+++ src/app/field-editor/field-editor.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { JsonProviderService } from '../json-provider/json-provider.service'
 
 @Component({
-  selector: 'app-field-editor',
+  selector: 'field-editor',
   templateUrl: './field-editor.component.html',
   styleUrls: ['./field-editor.component.css']
 })
@@ -10,6 +10,8 @@ export class FieldEditorComponent implements OnInit {
   private filterKeys = [];
   private isSelected = false;
   private currentKey: string;
+  @ViewChild("updateInput") updateInput;
+
   constructor(private jsonService: JsonProviderService) { }
 
   ngOnInit() {
@@ -40,6 +42,8 @@ export class FieldEditorComponent implements OnInit {
         this.currentKey = "";
         this.isSelected = false;        
       }
+
+      this
       this.jsonService.addFilterKey(this.currentKey);
     }    
   }
@@ -64,6 +68,7 @@ export class FieldEditorComponent implements OnInit {
     console.log(newValue);
     if(this.isSelected && this.currentKey != "") {
       this.jsonService.update(this.currentKey, newValue);
+      this.updateInput.nativeElement.value = "";
     }
   }
 }
